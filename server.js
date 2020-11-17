f67ab76c8fac6f2168765f0cfd132133cf3a40ba
Chad Altenberger <chadaltenberger@gmail.com>
2020-11-17 15:35:22 -0500

    updated server.js again

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express();
 // Set port
 const port = process.env.PORT || 3001;
 
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname + '/public'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+}
 
 // Set up routes
 app.get('/', (req, res) => {
