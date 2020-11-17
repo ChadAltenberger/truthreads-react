3e6e8df43f30ffc6cfc5e21fbd9ff44475f4337f
Chad Altenberger <chadaltenberger@gmail.com>
2020-11-17 14:55:48 -0500

    updated port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // Set port
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 
 app.use(express.static(__dirname));
 
