7417fd90887741f2ca8ceeb8e1e7caa3f3c594c9
Chad Altenberger <chadaltenberger@gmail.com>
2021-01-31 18:31:51 -0500

    Fixed alignment of featured work cards and two cards on home page

--- src/components/FeaturedWork.js
+++ src/components/FeaturedWork.js
@@ -1,88 +1,88 @@
 import React, { Component } from 'react';
 import {
-  Card,
-  CardImg,
-  CardTitle,
-  CardBody,
-  CardText,
-  Modal,
-  ModalBody,
-  ModalHeader
+	Card,
+	CardImg,
+	CardTitle,
+	CardBody,
+	CardText,
+	Modal,
+	ModalBody,
+	ModalHeader
 } from 'reactstrap';
 import { FEATURES } from '../shared/features';
 
 class FeaturedWork extends Component {
-  componentDidMount() {
-    window.scrollTo(0, 0);
-  }
+	componentDidMount() {
+		window.scrollTo(0, 0);
+	}
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      features: FEATURES,
-      selectedFeature: null,
-      isModalOpen: false
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			features: FEATURES,
+			selectedFeature: null,
+			isModalOpen: false
+		};
+	}
 
-  onFeatureSelect = feature => {
-    this.setState({ selectedFeature: feature });
-    this.setState({ isModalOpen: !this.state.isModalOpen });
-  };
+	onFeatureSelect = feature => {
+		this.setState({ selectedFeature: feature });
+		this.setState({ isModalOpen: !this.state.isModalOpen });
+	};
 
-  renderSelectedFeature(feature) {
-    if (feature) {
-      return (
-        <Card>
-          <CardImg top src={feature.image} alt={feature.name} />
-          <CardBody>
-            <CardTitle className='lead text-center'>
-              <strong>{feature.name}</strong>
-            </CardTitle>
-            <hr />
-            <CardText className='text-center'>{feature.details}</CardText>
-          </CardBody>
-        </Card>
-      );
-    }
-    return <div />;
-  }
+	renderSelectedFeature(feature) {
+		if (feature) {
+			return (
+				<Card>
+					<CardImg top src={feature.image} alt={feature.name} />
+					<CardBody>
+						<CardTitle className='lead text-center'>
+							<strong>{feature.name}</strong>
+						</CardTitle>
+						<hr />
+						<CardText className='text-center'>{feature.details}</CardText>
+					</CardBody>
+				</Card>
+			);
+		}
+		return <div />;
+	}
 
-  render() {
-    const featDisplay = this.state.features.map(feature => {
-      return (
-        <div id='featureImage' key={feature.id} className='col-md-5 m-1'>
-          <Card id='displayCard' onClick={() => this.onFeatureSelect(feature)}>
-            <CardImg
-              id='featureImage'
-              className='top'
-              src={feature.image}
-              alt={feature.name}
-            />
-            <CardTitle className='lead text-center mb-0'>
-              <strong>{feature.name}</strong>
-            </CardTitle>
-          </Card>
-        </div>
-      );
-    });
+	render() {
+		const featDisplay = this.state.features.map(feature => {
+			return (
+				<div id='featureImage' key={feature.id} className='col-md-5 m-1'>
+					<Card id='displayCard' onClick={() => this.onFeatureSelect(feature)}>
+						<CardImg
+							id='featureImage'
+							className='top'
+							src={feature.image}
+							alt={feature.name}
+						/>
+						<CardTitle className='lead text-center mb-0'>
+							<strong>{feature.name}</strong>
+						</CardTitle>
+					</Card>
+				</div>
+			);
+		});
 
-    return (
-      <div className='container'>
-        <div className='row'>{featDisplay}</div>
-        <Modal isOpen={this.state.isModalOpen} toggle={this.onFeatureSelect}>
-          <ModalHeader
-            id='featureModalHeader'
-            className='border-0'
-            toggle={this.onFeatureSelect}
-          ></ModalHeader>
-          <ModalBody>
-            {this.renderSelectedFeature(this.state.selectedFeature)}
-          </ModalBody>
-        </Modal>
-      </div>
-    );
-  }
+		return (
+			<div className='container'>
+				<div className='row featureContainer'>{featDisplay}</div>
+				<Modal isOpen={this.state.isModalOpen} toggle={this.onFeatureSelect}>
+					<ModalHeader
+						id='featureModalHeader'
+						className='border-0'
+						toggle={this.onFeatureSelect}
+					></ModalHeader>
+					<ModalBody>
+						{this.renderSelectedFeature(this.state.selectedFeature)}
+					</ModalBody>
+				</Modal>
+			</div>
+		);
+	}
 }
 
 export default FeaturedWork;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,81 +3,81 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 export default () => {
-  const MissionCard = () => {
-    return (
-      <Card className='mt-4'>
-        <CardImg
-          id='mission'
-          top
-          src='/assets/images/mission-logo.png'
-          alt='Mission Image'
-        />
-        <CardBody>
-          <CardText>
-            <p className='lead'>
-              Delivering quality and creativity since 2013. With people at the
-              heart of what we do, our passion is serving, not selling.
-            </p>
-            <hr />
-            <p className='text-center'>
-              Find out more about who we are and what we do.
-            </p>
-            <div id='buttonDiv'>
-              <button type='button' className='btn btn-outline-dark btn-lg'>
-                <Link className='link text-center' to='/about-us'>
-                  Find out more
-                </Link>
-              </button>
-            </div>
-          </CardText>
-        </CardBody>
-      </Card>
-    );
-  };
+	const MissionCard = () => {
+		return (
+			<Card className='mt-4' homeCard>
+				<CardImg
+					id='mission'
+					top
+					src='/assets/images/mission-logo.png'
+					alt='Mission Image'
+				/>
+				<CardBody className='cardBody'>
+					<CardText>
+						<p className='lead'>
+							Delivering quality and creativity since 2013. With people at the
+							heart of what we do, our passion is serving, not selling.
+						</p>
+						<hr />
+						<p className='text-center'>
+							Find out more about who we are and what we do.
+						</p>
+						<div id='buttonDiv'>
+							<button type='button' className='btn btn-outline-dark btn-lg'>
+								<Link className='link text-center' to='/about-us'>
+									Find out more
+								</Link>
+							</button>
+						</div>
+					</CardText>
+				</CardBody>
+			</Card>
+		);
+	};
 
-  const FeaturedCard = () => {
-    return (
-      <Card className='mt-4'>
-        <CardImg
-          id='featured'
-          top
-          src='/assets/images/o-grave.png'
-          alt='O Grave Shirt'
-        />
-        <CardBody>
-          <CardTitle className='text-center'>
-            <strong>Check out some of our work.</strong>
-          </CardTitle>
-          <CardText>
-            <p className='lead'>
-              Browse photos of some of our featured work to get a feel for what
-              we do.
-            </p>
-            <hr />
-            <div id='buttonDiv'>
-              <button type='button' className='btn btn-outline-dark btn-lg'>
-                <Link className='link' to='/featured'>
-                  Browse
-                </Link>
-              </button>
-            </div>
-          </CardText>
-        </CardBody>
-      </Card>
-    );
-  };
-  return (
-    <>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-6 mr-10'>
-            <MissionCard />
-          </div>
-          <div className='col-md-6 ml-10'>
-            <FeaturedCard />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	const FeaturedCard = () => {
+		return (
+			<Card className='mt-4' homeCard>
+				<CardImg
+					id='featured'
+					top
+					src='/assets/images/o-grave.png'
+					alt='O Grave Shirt'
+				/>
+				<CardBody className='cardBody2'>
+					<CardTitle className='text-center'>
+						<strong>Check out some of our work.</strong>
+					</CardTitle>
+					<CardText>
+						<p className='lead'>
+							Browse photos of some of our featured work to get a feel for what
+							we do.
+						</p>
+						<hr />
+						<div id='buttonDiv'>
+							<button type='button' className='btn btn-outline-dark btn-lg'>
+								<Link className='link' to='/featured'>
+									Browse
+								</Link>
+							</button>
+						</div>
+					</CardText>
+				</CardBody>
+			</Card>
+		);
+	};
+	return (
+		<>
+			<div className='container'>
+				<div className='row'>
+					<div className='col-md-6 mr-10'>
+						<MissionCard />
+					</div>
+					<div className='col-md-6 ml-10'>
+						<FeaturedCard />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
